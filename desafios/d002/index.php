424d5f7c311be8d266e1396c7bd029111739f142
Nicholas Emery <nicholasemery10@gmail.com>
2023-03-23 15:45:25 -0300

    Update 23/03/23 15:45

---
+++ desafios/d002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Trabalhando com Números Aleatórios</h1>
+        <p>Gerando um Número Aleatório entre 0 e 100.</p>
+        <?php
+            echo ""
+        ?>
+        <button type="submit" name="random">Gerar Outro</button>
+    </main>
+</body>
+</html>
